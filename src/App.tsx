81682d4f6e8a3ee50031c8c3e9fc8ba013547c6b
Timothy-Mutia <20210023599@my.xu.edu.ph>
2024-08-29 21:20:48 +0800

    feat: added initial structure for my static homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import Helloworld from './Helloworld';
 import './App.css'
+import Menu from './Components/Menu';
 
 function App() {
 
   return (
-     <Helloworld/>
+      <div className="App">
+        <div className="Hero">
+          <Menu></Menu>
+        </div>
+        <div className='Text_Section'>
+          <div>
+            <h1 className='H1'>I am Atomic</h1>
+
+          </div>
+
+          <div>
+            <h2 className='H2'>I love Atoms</h2>
+            <p className='paragraph'>
+              Lorem Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Temporibus, quasi. Explicabo 
+              aspernatur excepturi autem nihil molestiae! 
+              Similique accusamus totam ullam deserunt placeat,
+              alias, adipisci nisi impedit, animi veritatis autem provident.
+              Lorem Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Temporibus, quasi. Explicabo 
+              aspernatur excepturi autem nihil molestiae! 
+              Similique accusamus totam ullam deserunt placeat,
+              alias, adipisci nisi impedit, animi veritatis autem provident.
+              Lorem Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Temporibus, quasi. Explicabo 
+              aspernatur excepturi autem nihil molestiae! 
+              Similique accusamus totam ullam deserunt placeat,
+              alias, adipisci nisi impedit, animi veritatis autem provident.
+            </p>
+          </div>
+
+        </div>
+      </div>
   );
 }
 
 export default App
+
+
+//#FAEF00
+//#FCBF7E
+//#3B00FC
+//#FA004F
+//#72EBFC
